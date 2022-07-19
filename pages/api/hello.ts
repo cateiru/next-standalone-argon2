fd38e47162621669fdd27a8667788a3127b0fde9
Yuto Watanabe <yuto.w51942@gmail.com>
2022-07-19 19:32:31 +0900

    add argon2

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,11 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import argon2 from 'argon2'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-type Data = {
-  name: string
-}
-
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<string>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const hashed = await argon2.hash("password")
+
+  res.send(hashed)
 }
